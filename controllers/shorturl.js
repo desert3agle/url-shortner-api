5fcda107a928c33daf425ff8a4f7f1e8f3591167
Harsh Vardhan <harsh.vardhan320@gmail.com>
2021-02-27 15:55:55 +0530

    regex fix

--- controllers/shorturl.js
+++ controllers/shorturl.js
@@ -1,4 +1,5 @@
 const Url = require('../models/Url');
+const dns = require('dns');
 
 exports.createShortUrl = async (req, res) => {
     try {
@@ -10,9 +11,12 @@ exports.createShortUrl = async (req, res) => {
 
         /**
          * @description      validation test
+         * 
          */
-        let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+        let urlRegex = new RegExp(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi);
         if (!input_url.match(urlRegex)) return res.status(400).json({ error: 'invalid url' });
+
+
 
         /**
          * @description      duplication check
