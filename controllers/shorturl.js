968a8cc265f3bf4118626b1ed26dc1b4afcc30e5
Harsh Vardhan <harsh.vardhan320@gmail.com>
2021-02-27 13:27:17 +0530

    regex fix

--- controllers/shorturl.js
+++ controllers/shorturl.js
@@ -5,12 +5,14 @@ exports.createShortUrl = async (req, res) => {
 
         if (!req.body.url) return res.status(400).send('BAD REQUEST');
 
+        let input_url = req.body.url;
+        let responseObject = {};
+
         /**
          * @description      validation test
          */
-        const isValid = req.body.url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-        if (!isValid) return res.status(406).send('NOT ACCEPTABLE');
-
+        let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+        if (!input_url.match(urlRegex)) return res.status(400).json({ error: 'invalid url' });
 
         /**
          * @description      duplication check
@@ -41,12 +43,15 @@ exports.createShortUrl = async (req, res) => {
          */
 
         const newUrl = new Url({
-            orginal_url: req.body.url,
+            orginal_url: input_url,
             short_url: short_url
         });
         const savedData = await newUrl.save();
 
-        res.status(201).json(savedData);
+        responseObject['orginal_url'] = savedData.orginal_url;
+        responseObject['short_url'] = savedData.short_url;
+
+        res.status(201).json(responseObject);
     } catch (err) {
         res
             .status(err.status || 500)
@@ -59,7 +64,7 @@ exports.createShortUrl = async (req, res) => {
 exports.redirectToUrl = async (req, res) => {
     try {
         const response = await Url.findOne({ short_url: req.params.url });
-        if (!response) return res.status(404).json({ error: 'invalid url' });
+        if (!response) return res.status(404).json({ error: 'NOT FOUND' });
         res.status(302).redirect(response.orginal_url);
 
     } catch (err) {
